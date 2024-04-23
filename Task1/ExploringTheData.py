4c0c6e8af516391d045a94a568c13d5503249f7c
JalmaGit <jmbb00@hotmail.com>
2024-04-23 08:01:01 +0200

    Update

--- Task1/ExploringTheData.py
+++ Task1/ExploringTheData.py
@@ -1,13 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import DataFrameHandler as DFH
 
 fileName = "WineQT.csv"
 
 dataFrame = pd.read_csv(fileName)
 
-print(dataFrame.describe())
+dataFrame = dataFrame.drop_duplicates()
+
+dataFrame = dataFrame.dropna(axis=0, how='all')
 
 for i in range(11):
     print("When quality is",i,", the number of wines with said quality is:",(dataFrame["quality"] == i).sum())
 
-print(dataFrame)
+print(dataFrame.describe())
+
+print(np.std(dataFrame["quality"]))
+
+#plt.boxplot(dataFrame["alcohol"])
+plt.scatter(dataFrame["citric acid"], dataFrame["fixed acidity"], color="blue", alpha=0.5)
+plt.show()
