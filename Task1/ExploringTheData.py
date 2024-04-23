9393bd6ee9320e2bcd84e1b79e02ad9860b2c605
JalmaGit <jmbb00@hotmail.com>
2024-04-23 18:09:26 +0200

    Upodate

--- Task1/ExploringTheData.py
+++ Task1/ExploringTheData.py
@@ -11,13 +11,25 @@ dataFrame = dataFrame.drop_duplicates()
 
 dataFrame = dataFrame.dropna(axis=0, how='all')
 
+objectsBelongingToEachClass = np.empty(0)
+
 for i in range(11):
+    objectsBelongingToEachClass = np.append(objectsBelongingToEachClass,(dataFrame["quality"] == i).sum())
     print("When quality is",i,", the number of wines with said quality is:",(dataFrame["quality"] == i).sum())
+
+labels = np.array(["Disgusting","Extremly Bad", "Very Bad", "Bad", "Kinda Bad", "Mediocre", "Decent", "Good", "Very Good", "Extremely Good", "Perfect"])
 
 print(dataFrame.describe())
 
 print(np.std(dataFrame["quality"]))
 
+dataFrame.hist(figsize=(14,8))
+
 #plt.boxplot(dataFrame["alcohol"])
-plt.scatter(dataFrame["citric acid"], dataFrame["fixed acidity"], color="blue", alpha=0.5)
+#plt.scatter(dataFrame["citric acid"], dataFrame["fixed acidity"], color="blue", alpha=0.5)
+#plt.figure(figsize=(8, 8))
+#plt.pie(objectsBelongingToEachClass, labels=None, autopct='%1.1f%%', startangle=140)
+#plt.title('Pie Chart')
+#plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#plt.legend(labels, loc="lower right")
 plt.show()
